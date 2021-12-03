d023f70a6b46e7bcb5488b99991d415f70936b10
Desktop-Jannik <jannik_walter@online.de>
2021-12-03 21:43:13 +0100

    tes test test

---
+++ src/main/java/com/stockie/Main.java
@@ -0,0 +1,37 @@
+package com.stockie;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Map;
+
+public class Main {
+    /*
+     * Entry-Point und Main der gesamten Applikation.
+     * */
+
+    // public Stockie(){}
+
+    public static void main(String[] args) throws IOException {
+
+        System.out.println("test");
+
+
+        // TEST ABFRAGE!
+        URLModel url = new URLModel("1A79MCHMT69G16RE","AAPL","5min","TIME_SERIES_INTRADAY");
+        API api = new API();
+        String alphaVantageUrl = url.getUrl();
+        String response = api.getWebPage(alphaVantageUrl);
+        ArrayList<Map<String, String>> data = api.getJson(response);
+
+        // DB Conector SELECT ABFRAGE UND UPLOAD API CALL
+        DatabaseConnector db = new DatabaseConnector();
+        db.viewData();
+
+        /** DIE UPLOAD METHODE ERSTMAL NICHT AUSFÜHREN !
+         MUSS NOCH ANGEPASST WERDEN
+         DATEN IN DER DB WÜRDEN SONST DOPPELT VORHANDEN SEIN.
+         */
+        db.uploadData(data);
+
+
+    }
+}
