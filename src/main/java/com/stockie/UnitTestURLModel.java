53401002978192c1fdf2ea34bfb7543be214680f
Oliver Isaak <oliver.9002s@gmail.com>
2021-12-17 14:17:05 +0100

    JunitTest

--- src/main/java/com/stockie/UnitTestURLModel.java
+++ src/main/java/com/stockie/UnitTestURLModel.java
@@ -9,11 +9,13 @@ import java.util.ArrayList;
 import java.util.LinkedHashMap;
 
     public class UnitTestURLModel {
+        URLModel url = new URLModel("1A79MCHMT69G16RE", "1min");
+        MockData urlMock = new MockData();
         @Test
         public void getAssetUrlsUnitTest(){
 
-            URLModel url = new URLModel("1A79MCHMT69G16RE", "1min");
-            MockData urlMock = new MockData();
+
+
 
             LinkedHashMap<String, String> actualUrls = url.getAssetUrls();
             ArrayList<String> expectedUrls = urlMock.getUrlMock();
@@ -23,6 +25,24 @@ import java.util.LinkedHashMap;
             {
                 System.out.println(actualUrls.get(keys));
                 assertEquals(expectedUrls.get(i),actualUrls.get(keys) );
+                i++;
+            }
+
+        }
+        @Test
+        public void getAssetUrlsHistoryUnitTest(){
+
+
+
+
+            LinkedHashMap<String, String> actualHistoryUrls = url.getAssetUrlsHistroy();
+            ArrayList<String> expectedHistoryUrls = urlMock.getHistoryUrlMock();
+
+            int i = 0;
+            for (String keys : actualHistoryUrls.keySet())
+            {
+                System.out.println(actualHistoryUrls.get(keys));
+                assertEquals(expectedHistoryUrls.get(i),actualHistoryUrls.get(keys) );
                 i++;
             }
 
