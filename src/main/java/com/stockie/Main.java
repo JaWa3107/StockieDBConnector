f554df904d2f7a03500b2ed26a3c87e751228292
Desktop-Jannik <jannik317@googlemail.com>
2021-12-23 18:04:50 +0100

    add maven surefire

--- src/main/java/com/stockie/Main.java
+++ src/main/java/com/stockie/Main.java
@@ -13,16 +13,16 @@ public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
 
         Config config = new Config();
-        URLModel urlModel = new URLModel(config.getKey("Alphakey"),config.getInterval("Interval"),config.getAssets("Assets"));
+        URLModel urlModel = new URLModel(config.getKey("Alphakey"),config.getInterval("minute"),config.getAssets("Assets"));
         API api = new API();
         DatabaseConnector DbConn = new DatabaseConnector();
         Routine routine = new Routine(urlModel,DbConn,api);
 
         System.out.println("Starting Upload");
 
-        routine.init(config.getInterval("Interval"), config.getTableName("TableName"));
+        routine.init(config.getIdentifier("minute"), config.getTableName("TableName"));
         Thread.sleep(60 * 1000); // Wait for 60 seconds
-        routine.init(config.getIdentifier("Identifier"), config.getTableName("HistoryTableName"));
+        routine.init(config.getIdentifier("daily"), config.getTableName("HistoryTableName"));
 
     }
 
